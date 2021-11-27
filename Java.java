e3db2e8f2892af9f6771cf87e3fd02f03a45e2dc
aravindbinu <85299497+aravindbinu@users.noreply.github.com>
2021-11-27 20:12:20 +0530

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Java {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the string");
+		String n = s.nextLine();
+		System.out.println(n);
+		
+
+	}
+
+}
